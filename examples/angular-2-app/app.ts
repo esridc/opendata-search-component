7c7680361c5951424f652ec47edbac24d292b200
Patrick Arlt <patrick.arlt@gmail.com>
2015-08-28 16:32:44 -0700

    angular 1 app

---
+++ examples/angular-2-app/app.ts
@@ -0,0 +1,33 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+// Annotation section
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: `
+    <h1>My Item</h1>
+    <item-rating
+      itemid="{{itemId}}"
+      rating="{{rating}}"
+      numratings="{{numRatings}}"
+      (rateitem)="rateItem($event)"></item-rating>`
+})
+// Component controller
+class MyAppComponent {
+  rating: number;
+  itemId: string;
+  numRatings: number;
+
+  constructor() {
+    this.itemId = '30e5fe3149c34df1ba922e6f5bbf808f';
+    this.rating = 4.25;
+    this.numRatings = 6;
+  }
+
+  rateItem ($event) {
+    console.log('Rating In Angular:', $event.detail.rating);
+  }
+}
+
+bootstrap(MyAppComponent);
