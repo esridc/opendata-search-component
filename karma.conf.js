1059d43394e8aa05dcad2d878a094794e83a2e65
mjuniper <mjuniper@gmail.com>
2015-10-15 12:30:31 -0600

    really kill jasmine-jquery

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'browserify', 'jasmine-jquery', 'jasmine' ],
+    frameworks: [ 'browserify', 'jasmine' ],
 
     browserify: {
       debug: true,
